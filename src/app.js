9591be2a2537db0f95eedd4da45e2ca79bd5dba6
pandabrand <pandabrand@gmail.com>
2018-05-06 10:24:12 -0500

    converted to using WebAudio API

--- src/app.js
+++ src/app.js
@@ -2,19 +2,36 @@ import React, { Component } from 'react';
 import './app.css';
 import DrumKey from'./keys.js';
 
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
 class App extends Component {
 
     componentDidMount() {
         window.addEventListener( 'keydown', this.playSound );
+        sounds.forEach((obj) => {
+            var request = new XMLHttpRequest();
+            request.open('GET', 'sounds/'+obj.sound, true);
+            request.responseType = 'arraybuffer';
+          
+            // Decode asynchronously
+            request.onload = function() {
+                audioContext.decodeAudioData(request.response, function(buffer) {
+                obj.buffer = buffer;
+              }, function(e){ console.log("Error with decoding audio data" + e.err); });
+            }
+            request.send();
+        });
     }
 
     playSound( e ) {
         const key = document.querySelector( `.key[data-key="${e.keyCode}"]` );
-        const audio = document.querySelector( `audio[data-key="${e.keyCode}"]` );
-        if( !audio ) return;
-
-        audio.currentTime = 0;
-        audio.play();
+        if(!key)
+            return;
+        const audio = sounds.find( (obj) => obj.kCode === e.keyCode );
+        var source = audioContext.createBufferSource(); // creates a sound source
+        source.buffer = audio.buffer;                    // tell the source which sound to play
+        source.connect(audioContext.destination);       // connect the source to the context's destination (the speakers)
+        source.start(0);                           // play the source now
         key.classList.add('playing');
     }
 
@@ -32,15 +49,15 @@ class App extends Component {
 }
 
 const sounds = [
-    {kCode:'65', sound:'clap.wav', name:'clap', letter:'A'},
-    {kCode:'83', sound:'hihat.wav', name:'hihat', letter:'S'},
-    {kCode:'68', sound:'kick.wav', name:'kick', letter:'D'},
-    {kCode:'70', sound:'openhat.wav', name:'openhat', letter:'F'},
-    {kCode:'71', sound:'boom.wav', name:'boom', letter:'G'},
-    {kCode:'72', sound:'ride.wav', name:'ride', letter:'H'},
-    {kCode:'74', sound:'snare.wav', name:'snare', letter:'J'},
-    {kCode:'75', sound:'tom.wav', name:'tom', letter:'K'},
-    {kCode:'76', sound:'tink.wav', name:'tink', letter:'L'}
+    {kCode: 65, sound:'clap.wav', name:'clap', letter:'A'},
+    {kCode: 83, sound:'hihat.wav', name:'hihat', letter:'S'},
+    {kCode: 68, sound:'kick.wav', name:'kick', letter:'D'},
+    {kCode: 70, sound:'openhat.wav', name:'openhat', letter:'F'},
+    {kCode: 71, sound:'boom.wav', name:'boom', letter:'G'},
+    {kCode: 72, sound:'ride.wav', name:'ride', letter:'H'},
+    {kCode: 74, sound:'snare.wav', name:'snare', letter:'J'},
+    {kCode: 75, sound:'tom.wav', name:'tom', letter:'K'},
+    {kCode: 76, sound:'tink.wav', name:'tink', letter:'L'}
 ];
 
 export default App;
